eeec341f30348b283cf7e9d141440c551fdf5d65
Hilario-C <79656188+Hilario-C@users.noreply.github.com>
2021-11-10 14:55:09 -0600

    added upgrade logic

--- Cyber_Smash/Assets/Player_Movement.cs
+++ Cyber_Smash/Assets/Player_Movement.cs
@@ -6,25 +6,54 @@ public class Player_Movement : MonoBehaviour
     public float speed;
     private float Left_Right;
     public float jumpForce;
+    public float upgradeTimer;
+    public float upgradeSpeed;
+    public bool upgraded = false;
 
-    
+  
+
+
 
     private void FixedUpdate()
     {
         // Player jumps
-        if ( Input.GetKey("w"))
+        if (Input.GetKey("w"))
         {
-            
+            Debug.Log("jump");
             body.velocity = Vector2.up * jumpForce * 100 * Time.deltaTime;
         }
 
-        // player moves left to right
-        body.velocity = new Vector2(Left_Right * speed *100 * Time.deltaTime, body.velocity.y);
-      
+        if (!upgraded)
+        {
+            // player moves left to right
+            body.velocity = new Vector2(Left_Right * speed * 100 * Time.deltaTime, body.velocity.y);
+
+        }
+        else
+        {
+            upgradeTimer = Time.deltaTime;
+            if (upgradeTimer >= 5)
+            {
+                Debug.Log("downgrade");
+                upgraded = false;
+                upgradeTimer = 0;
+            }
+            Debug.Log("upgraded");
+            body.velocity = new Vector2(Left_Right * upgradeSpeed * 100 * Time.deltaTime, body.velocity.y);
+
+        }
+
+
     }
 
     private void Update()
     {
-        Left_Right = Input.GetAxisRaw("Horizontal");   
+        Left_Right = Input.GetAxisRaw("Horizontal");
+    }
+
+    public void MoveFaster()
+    {
+        upgraded = true;
+
     }
 }
--- Cyber_Smash/Assets/Upgrade.cs
+++ Cyber_Smash/Assets/Upgrade.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Upgrade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Player")
+        {
+            
+
+            collision.GetComponent<Player_Movement>().MoveFaster();
+            gameObject.SetActive(false);
+            Debug.Log("been grabbed");
+        }
     }
 }
