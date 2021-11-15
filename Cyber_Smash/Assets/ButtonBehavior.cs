1f1176887027529f3fce4b641ff03b1689304a49
TristinMcClelland <73961414+TristinMcClelland@users.noreply.github.com>
2021-11-15 13:33:56 -0600

    WinScreen
    Added the win screen and the script for it

---
+++ Cyber_Smash/Assets/ButtonBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonBehavior : MonoBehaviour
+{
+
+    public void Update()
+    {
+        Debug.Log("Button Pressed");
+
+        //Reload The Scene Here
+
+    }
+}
